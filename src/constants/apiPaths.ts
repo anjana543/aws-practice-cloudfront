f33b7ba0ca444ae5ec6e00642e1c6ed39b4fbb35
V-EPAM-Anjana Nair <V-EPAM-Anjana.Nair@metrolinx.com>
2024-04-10 17:10:32 +0530

    update import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://6noo2y8h65.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://6noo2y8h65.execute-api.us-east-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://meuq7tp91b.execute-api.us-east-1.amazonaws.com/dev/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
